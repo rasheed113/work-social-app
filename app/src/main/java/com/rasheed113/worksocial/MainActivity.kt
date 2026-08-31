7d55361dfaf1a72b7af4a420d8fad47368e205be
Rasheed Gull <140941093+rasheed113@users.noreply.github.com>
2026-08-31 19:42:10 +0500

    fix: wire Supabase Android auth extensions

--- app/src/main/java/com/rasheed113/worksocial/MainActivity.kt
+++ app/src/main/java/com/rasheed113/worksocial/MainActivity.kt
@@ -8,6 +8,8 @@ import com.rasheed113.worksocial.infrastructure.auth.SupabaseAuthRepository
 import com.rasheed113.worksocial.presentation.auth.AuthViewModel
 import com.rasheed113.worksocial.presentation.auth.AuthViewModelFactory
 import com.rasheed113.worksocial.presentation.ui.WorkSocialApp
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.handleDeeplinks
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
