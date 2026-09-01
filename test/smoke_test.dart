2acd04c7157b1325a76050d564c13ef9e8d800d7
Rasheed Gull <140941093+rasheed113@users.noreply.github.com>
2026-09-01 15:55:17 +0500

    test: use centralized design theme

--- test/smoke_test.dart
+++ test/smoke_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:work_social_app/app/theme/app_theme.dart';
+import 'package:work_social_app/core/design/app_theme.dart';
+import 'package:work_social_app/core/design/app_tokens.dart';
 
 void main() {
-  test('Work Social theme exposes expected design tokens', () {
+  test('Work Social theme exposes centralized design tokens', () {
     final theme = AppTheme.light();
+
     expect(theme.useMaterial3, isTrue);
-    expect(theme.scaffoldBackgroundColor, AppTheme.background);
+    expect(theme.scaffoldBackgroundColor, AppTokens.pageBackground);
+    expect(AppTokens.radiusCard, 22.0);
+    expect(AppTokens.radiusHero, 26.0);
   });
 }
